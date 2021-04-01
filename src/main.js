41de2529f2856ff7e041ad2078c723c22f756e0b
Kelsey Cahill <kcahill@southleft.com>
2021-04-01 19:55:32 -0400

    Performance updates.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import lazyPlugin from 'vue3-lazy'
 import './styles/styles.scss'
 
-const app = createApp(App)
-app.use(lazyPlugin, {
-  loading: 'loading.png',
-  error: 'error.png'
-})
-app.mount('#app')
+createApp(App).mount('#app')
